1bdd7ee276a167fd3ca0490e5faf91fc76a8d5f0
msp <info@radialsolutions.co.uk>
2011-04-04 20:36:11 +0100

    Make section mandatry

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,7 +2,7 @@ class Page < ActiveRecord::Base
   YEAR = Page.all.map{|a| a.year }.uniq.sort
 #  SECTION = Page.all.map{|a| !a.section.blank? ? a.section : "" }.uniq.sort
 
-  validates_presence_of :name, :slug, :year
+  validates_presence_of :name, :slug, :year, :section
   has_many :content_areas
   has_and_belongs_to_many :assets
 
