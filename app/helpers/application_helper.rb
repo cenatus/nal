850e5cbd041771a946f7f89bd353ecf079981aa6
andi <andi@portaputer.lan>
2011-04-18 16:23:42 +0100

    - artist page shows homepage and twitter link if available   - if both available they are seperated by a | -all images are a link by default without hover text   - settings can be altered by passing different parameters to the article_image function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,16 +20,44 @@ module ApplicationHelper
   end
 
 
-  def article_image(klass)
+  def article_image(klass, options={:hover_flag => false, :link_flag =>true})
     return "" if (!klass.respond_to?(:assets) || klass.assets.blank?)
     class_slug = klass.class.to_s == "NewsItem" ? "News" : klass.class.to_s
     year = klass.class.to_s == "NewsItem" ? "" : "#{Nal::Application::CURRENT_YEAR}/"
 
-    haml_tag "a", {:href => "/#{year}#{class_slug.downcase}/#{klass.friendly_id}"} do
-      haml_tag "IMG.article_image", :src => klass.assets.first.asset.url
+    if options[:hover_flag] == true
+      #sliding boxes plugin
+      haml_tag "div.boxgrid.captionfull" do
+        if options[:link_flag] == true
+          haml_tag "a", {:href => "/#{year}#{class_slug.downcase}/#{klass.friendly_id}"} do
+            display_img klass
+          end
+        else
+          display_img klass
+        end
+
+        if !klass.assets.first.description.blank?
+          haml_tag "div.cover.boxcaption" do
+            haml_tag "h6" do
+              haml_tag "p", klass.assets.first.description
+            end
+          end
+        end
+      end
+    else
+      if options[:link_flag] == true
+        haml_tag "a", {:href => "/#{year}#{class_slug.downcase}/#{klass.friendly_id}"} do
+          display_img klass
+        end
+      else
+        display_img klass
+      end
     end
   end
 
+  def display_img (klass)
+    haml_tag "IMG.article_image", :src => klass.assets.first.asset.url
+  end
   def article_thumb(klass)
     return "" if (!klass.respond_to?(:assets) || klass.assets.blank?)
     class_slug = klass.class.to_s == "NewsItem" ? "News" : klass.class.to_s
