754e9aa1c7f368188addafbf160a0f42fc014f63
andi <andi@portaputer.lan>
2011-04-26 11:08:06 +0100

    -refactored the search to display results in a structured manner  - search now also checks all content texts of events, news, etc

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,13 @@
+class SearchController < ApplicationController
+  def search
+
+
+    # Pass any of the ActiveRecord find options to the search.
+    @results_strands = Strand.find_with_index(params[:query],{:limit => 50})
+    @results_news = NewsItem.find_with_index(params[:query],{:limit => 50})
+    @results_events = Event.find_with_index(params[:query],{:limit => 50})
+    @results_parts = Participant.find_with_index(params[:query],{:limit => 50})
+
+
+  end
+end
