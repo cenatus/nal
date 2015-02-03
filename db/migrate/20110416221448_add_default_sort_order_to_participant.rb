092dbc343111048fb7c0c38201b7abf0335df5db
andistuder <andi@cenatus.org>
2015-02-03 21:58:34 +0000

    update migration to avoid deprecated methods

--- db/migrate/20110416221448_add_default_sort_order_to_participant.rb
+++ db/migrate/20110416221448_add_default_sort_order_to_participant.rb
@@ -1,10 +1,8 @@
 class AddDefaultSortOrderToParticipant < ActiveRecord::Migration
   def self.up
     change_column_default(:participants, :sort_order, 0)
-    Participant.reset_column_information
-    Participant.find(:all).each do |p|
-      p.update_attribute :sort_order, 0 if p.sort_order.blank?
-    end
+
+    Participant.connection.execute('UPDATE participants SET sort_order=0 WHERE sort_order is null')
   end
 
   def self.down
